00bf4538b1ff5d1f78df8a6a945b4cc998742262
unknown <surinderpalsingh001@outlook.com>
2023-02-14 20:10:11 +0530

    login form

--- view/user/userloginform.php
+++ view/user/userloginform.php
@@ -108,10 +108,6 @@ if (isset($_POST['login']))
             <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="username">
           </div>
             <div class="mb-3">
-              <label for="exampleInputEmail1" class="form-label">Email address</label>
-              <input type="email" class="form-control" id="exampleInputEmail1" name="email" aria-describedby="emailHelp">
-            </div>
-            <div class="mb-3">
               <label for="exampleInputPassword1" class="col-form-label">Password</label>
               <input type="password" class="form-control" id="exampleInputPassword1" name="password">
             </div>
