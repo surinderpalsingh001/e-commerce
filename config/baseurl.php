205a6c0e1790f1844eeb8e7b02b4fa878d722c2d
unknown <surinderpalsingh001@outlook.com>
2023-02-15 11:49:51 +0530

    change

--- config/baseurl.php
+++ config/baseurl.php
@@ -1,5 +1,5 @@
 <?php
-    $baseUrl = 'http://'.$_SERVER['SERVER_NAME']."/e-commerce/";
+    $baseUrl = 'http://'.$_SERVER['SERVER_NAME'];
     function asset($src){
         global $baseUrl;
         return $baseUrl.'assets/upload_images/'.$src;
