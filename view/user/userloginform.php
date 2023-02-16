1f52ecaa163fa5d31d44e48f79acb5996054fe5c
unknown <surinderpalsingh001@outlook.com>
2023-02-16 21:08:02 +0530

    hellllooo

--- view/user/userloginform.php
+++ view/user/userloginform.php
@@ -1,87 +1,87 @@
 <?php
-// session_start();
-// if (isset($_POST['login']))
-//         {
-//             if(isset($_POST['username']) && isset($_POST['password']))
-//             {
-//                 $username = $_POST['username'];
-//                 $password = $_POST['password'];
+session_start();
+if (isset($_POST['login']))
+        {
+            if(isset($_POST['username']) && isset($_POST['password']))
+            {
+                $username = $_POST['username'];
+                $password = $_POST['password'];
         
-//                 $data = "username=".$username;
+                $data = "username=".$username;
             
-//                 if(empty($username))
-//                 {
-//                     dump($username);
-//                     $em = "User name is required";
-//                     header("Location:".$baseUrl."e-commerce/index.php/user/login?error=$em&$data");
-//                     exit;
-//                 }
-//                 else if(empty($password))
-//                 {
-//                     dump($password);
-//                     $em = "password is required";
-//                     header("Location:".$baseUrl."e-commerce/index.php/user/login?error=$em&$data");
-//                     header("Location:".$baseUrl."e-commerce//index.php/user/login?error=$em&$data");
-//                     exit;
-//                 }
-//                 else
-//                 {
-//                     $sql = "SELECT * FROM useradmindatas WHERE username = ? ";
-//                     $stmt = $dbh->prepare($sql);
-//                     $stmt->execute([$username]);
-//                     dump($stmt->rowCount());
-//                     if($stmt->rowCount() >= 1)
+                if(empty($username))
+                {
+                    dump($username);
+                    $em = "User name is required";
+                    header("Location:".$baseUrl."e-commerce/index.php/user/login?error=$em&$data");
+                    exit;
+                }
+                else if(empty($password))
+                {
+                    dump($password);
+                    $em = "password is required";
+                    header("Location:".$baseUrl."e-commerce/index.php/user/login?error=$em&$data");
+                    header("Location:".$baseUrl."e-commerce//index.php/user/login?error=$em&$data");
+                    exit;
+                }
+                else
+                {
+                    $sql = "SELECT * FROM useradmindatas WHERE username = ? ";
+                    $stmt = $dbh->prepare($sql);
+                    $stmt->execute([$username]);
+                    dump($stmt->rowCount());
+                    if($stmt->rowCount() >= 1)
             
-//                     {
+                    {
         
-//                         $users = $stmt->fetchAll();
-//                         $firstUser = null;
-//                         foreach ($users as $user) 
-//                         {
-//                           //dd($user);
-//                           if ($user['isadmin'] == '1' && password_verify($password,$user['password']))
-//                           {
-//                             $_SESSION['admin']['adm_id'] = $user['id'];
-//                             $_SESSION['admin']['username'] = $user['username'];
-//                             header("location:".$baseUrl."e-commerce/index.php/admin/admhome");
-//                             exit;
-//                           }
-//                           else if ($user['isadmin'] == '0' && password_verify($password,$user['password']))
-//                           {                            
-//                             $_SESSION['user']['user_id'] = $user['id'];
-//                             $_SESSION['user']['username'] = $user['username'];
-//                             header("location:".$baseUrl."e-commerce/index.php/user/profile");
-//                             exit;
-//                           }
-//                           else
-//                           {
-//                             $em = "Incorect User name or password";
-//                             header("Location:".$baseUrl." e-commerce/admlogin.php?error=$em&$data");
-//                             exit();
-//                           }
-//                         }
-//                         // // dump($user);
-//                         // $name =  $user['name'];
-//                         // $username =  $user['username'];
-//                         // $pass =  $user['password'];
-//                         // $id =  $user['id'];
-//                         // $image =  $user['image'];
+                        $users = $stmt->fetchAll();
+                        $firstUser = null;
+                        foreach ($users as $user) 
+                        {
+                          //dd($user);
+                          if ($user['isadmin'] == '1' && password_verify($password,$user['password']))
+                          {
+                            $_SESSION['admin']['adm_id'] = $user['id'];
+                            $_SESSION['admin']['username'] = $user['username'];
+                            header("location:".$baseUrl."e-commerce/index.php/admin/admhome");
+                            exit;
+                          }
+                          else if ($user['isadmin'] == '0' && password_verify($password,$user['password']))
+                          {                            
+                            $_SESSION['user']['user_id'] = $user['id'];
+                            $_SESSION['user']['username'] = $user['username'];
+                            header("location:".$baseUrl."e-commerce/index.php/user/profile");
+                            exit;
+                          }
+                          else
+                          {
+                            $em = "Incorect User name or password";
+                            header("Location:".$baseUrl." e-commerce/admlogin.php?error=$em&$data");
+                            exit();
+                          }
+                        }
+                        // // dump($user);
+                        // $name =  $user['name'];
+                        // $username =  $user['username'];
+                        // $pass =  $user['password'];
+                        // $id =  $user['id'];
+                        // $image =  $user['image'];
         
-//                         // if($username === $username)
-//                         // {
-//                         //    if(password_verify($password, $user['password']))
-//                         //    {
-//                         //         $_SESSION['image'] = $image;
-//                         //         $_SESSION['id'] = $id;
-//                         //         $_SESSION['name'] = $name;
-//                         //         $_SESSION['username'] = $username;
-//                         //         $_SESSION['email'] = $email;
-//                         //     }
-//                         // }
-//                     }
-//                 }
-//             }
-//         }
+                        // if($username === $username)
+                        // {
+                        //    if(password_verify($password, $user['password']))
+                        //    {
+                        //         $_SESSION['image'] = $image;
+                        //         $_SESSION['id'] = $id;
+                        //         $_SESSION['name'] = $name;
+                        //         $_SESSION['username'] = $username;
+                        //         $_SESSION['email'] = $email;
+                        //     }
+                        // }
+                    }
+                }
+            }
+        }
 ?>
 
 
